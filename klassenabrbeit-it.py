bc31bc402175bcb35de1e6665bf5ae24dac1c8f9
ferret_de <fh1234567890@outlook.de>
2025-12-06 01:35:48 +0100

    Grundgerüst

--- klassenabrbeit-it.py
+++ klassenabrbeit-it.py
@@ -1,26 +1,51 @@
+from threading import Thread
+from time import sleep
 Farben_Auto = [ "gruen" , "gelb", "rot", "rot+gelb", "Blinken"]
 Farben_Fuß = ["gruen", "rot", "aus"]
-night = true
+night = True
 
-class Auto_Apmel(object):
-     zustand = 0
-     Start_pose = 0
-     def __init__(self, Start_pos):
-	zustand = Start_pos
-	Start_pose = Start_pos
-	self.Farbe = Farben[Start_pos]
+class Auto_ampel(object):
+	def __init__(self, Start_pose):
+		self.zustand = Start_pose
+		self.Start_pos = Start_pose
+		self.Farbe = Farben_Auto[Start_pose]
+		self.update_thread = Thread(target=self.update(self))
+		self.update_thread.start()
 
-     def schalten(self):
-	if night == true:
-	  self.Farbe = Farben[4]
-	elif night == false:
-	  self.Farbe = Farben[Start_pose]
-	elif zustand = 3:
-	  zustand = 0
-	else:
-	 zustand += 1
+	def schalten(self):
+		if night == True:
+			self.Farbe = Farben_Auto[4]
+		elif night == False:
+			self.Farbe = Farben_Auto[self.Start_pos]
+		elif self.zustand == 3:
+			self.zustand = 0
+		else:
+			self.zustand += 1
 
-     def update(self):
-	self.Farbe = Farben[zustand]
+	def update(self):
+		while True:
+			self.Farbe = Farben_Auto[self.zustand]
+			sleep(0.5)
+
+
+class Fuß_ampel(object):
+
+	def __init__(self, Start_pose):
+		self.zustand = Start_pose
+		self.Start_pos = Start_pose
+		self.Farbe = Farben_Fuß[Start_pose]
+		self.update_thread = Thread(target=self.update(self))
+		self.update_thread.start()
+
+	def schalten(self):
+		if self.zustand == 2:
+			self.zustand = 0
+		else:
+			self.zustand += 1
+
+	def update(self):
+		while True:
+			self.Farbe = Farben_Fuß[self.zustand]
+			sleep(0.5)
 
 
