f101e8335a262bb1fe337501bdc7805477357bde
ferret_de <fh1234567890@outlook.de>
2025-12-06 01:55:03 +0100

    user interface

--- klassenabrbeit-it.py
+++ klassenabrbeit-it.py
@@ -48,4 +48,24 @@ class Fuß_ampel(object):
 			self.Farbe = Farben_Fuß[self.zustand]
 			sleep(0.5)
 
-
+def user_interface():
+	inti_art = input("Ampel Initialsierung Art (ENTER oder 'A' für Automatisch; 'M' für Manuel): ")
+	if inti_art == "A" or None:
+		Ampel_A_Nord_Süd = Auto_ampel(0)
+		Ampel_A_Süd_Nord = Auto_ampel(0)
+		Ampel_A_Ost_West = Auto_ampel(2)
+		Ampel_A_West_Ost = Auto_ampel(2)
+		Ampel_F_Nord_Süd = Fuß_ampel(1)
+		Ampel_F_Süd_Nord = Fuß_ampel(1)
+		Ampel_F_West_Ost = Fuß_ampel(0)
+		Ampel_F_Ost_West = Fuß_ampel(0)
+	elif inti_art == "M":
+		for _ in range(4): 
+			Name_ampel = input(f"Name der Auto_Ampel_{_+1}: ")
+			startzustand = input(f"{Name_ampel}s Start Position ('0' für gruen, '2' für rot): ")
+			Name_ampel = Auto_ampel(startzustand)
+		for _ in range(4): 
+			Name_ampel = input(f"Name der Fuß_Ampel_{_+1}: ")
+			startzustand = input(f"{Name_ampel}s Start Position ('0' für gruen. '1' für rot): ")
+			Name_ampel = Fuß_ampel(startzustand)
+user_interface()
